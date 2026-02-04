40002de4e1b6187f1ac4934f1ca5075bb0ebe6ee
SumedhMeshram <sumedhmeshram085@gmail.com>
2026-02-04 13:22:37 +0530

    Hello

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter first Number:");
+	scanf("%d",&a);
+	printf("Enter Second number:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("The sum of two numbers is %d\n",sum);
+}
